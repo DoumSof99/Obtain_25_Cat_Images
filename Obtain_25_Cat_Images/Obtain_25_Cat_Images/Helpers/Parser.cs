b80c6f8fc59539a4c2e83b12b1a748933c54f162
DoumSof99 <sofiadmn99@gmail.com>
2025-04-17 22:13:15 +0300

    Created parser for temperament and tests

---
+++ Obtain_25_Cat_Images/Obtain_25_Cat_Images/Helpers/Parser.cs
@@ -0,0 +1,13 @@
+﻿namespace Obtain_25_Cat_Images.Helpers {
+    public static class Parser {
+        public static List<string> ParseTemperament(string temperament) { 
+            if(string.IsNullOrWhiteSpace(temperament)) return [];
+
+            return temperament
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToList();
+        }
+    }
+}
---
+++ Obtain_25_Cat_Images/Obtain_25_Cats_Tests/ParseTemperamentTests.cs
@@ -0,0 +1,24 @@
+using Obtain_25_Cat_Images.Helpers;
+
+namespace Obtain_25_Cats_Tests {
+    public class ParseTemperamentTests {
+        
+        [Fact]
+        public void Parse_Temperament_Comma_Seperated_Correctly() {
+            var result = Parser.ParseTemperament("Active, Energetic, Independent, Intelligent");
+            Assert.Equal(["Active", "Energetic", "Independent", "Intelligent"], result);
+        }
+
+        [Fact]
+        public void Return_Empty_List_If_Null_Input() {
+            var result = Parser.ParseTemperament(null!);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Parse_Temperament_Trims_Whitespace() {
+            var result = Parser.ParseTemperament("    Active  ,   ,    Independent  ,    Intelligent");
+            Assert.Equal(["Active", "Independent", "Intelligent"], result);
+        }
+    }
+}
