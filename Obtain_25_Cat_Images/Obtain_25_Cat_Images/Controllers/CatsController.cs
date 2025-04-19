be60239dbf81f66bd27c83362043a295323bcd38
DoumSof99 <sofiadmn99@gmail.com>
2025-04-19 20:14:49 +0300

    created api documentation for swagger

--- Obtain_25_Cat_Images/Obtain_25_Cat_Images/Controllers/CatsController.cs
+++ Obtain_25_Cat_Images/Obtain_25_Cat_Images/Controllers/CatsController.cs
@@ -4,7 +4,17 @@ using Obtain_25_Cat_Images.Interfaces;
 
 namespace Obtain_25_Cat_Images.Controllers {
     public class CatsController(ICatService catService) : BaseApiController {
-        
+      
+        /// <summary>
+        /// Fetches 25 cat images with breed information and stores them in the database.
+        /// </summary>
+        /// <remarks>
+        /// This endpoint contacts TheCatAPI (https://thecatapi.com), extracts image + temperament data,
+        /// and stores cats with tag relations in SQL Server.
+        /// </remarks>
+        /// <returns>List of newly saved cat records</returns>
+        /// <response code="200">Returns the list of cats</response>
+        /// <response code="500">If the external API fails or saving to the DB fails</response>
         [HttpPost("fetch")]
         public async Task<IActionResult> FetchAndSaveCats() {
             var result = await catService.FetchAndSaveCatsAsync();
@@ -14,6 +24,15 @@ namespace Obtain_25_Cat_Images.Controllers {
                 : BadRequest(result.Errors[0].Message);
         }
 
+        /// <summary>
+        /// Retrieves a single cat by its database ID.
+        /// </summary>
+        /// <param name="id">The internal database ID of the cat.</param>
+        /// <returns>
+        /// </returns>
+        /// <response code="200">Returns the cat if found</response>
+        /// <response code="404">Returns a 404 if the cat is not found</response>
+        /// <response code="400">Returns a 400 if there was a request error</response>
         [HttpGet("{id}")]
         public async Task<IActionResult> GetCatById(int id) {
             var result = await catService.GetCatByIdAsync(id);
@@ -23,6 +42,15 @@ namespace Obtain_25_Cat_Images.Controllers {
                 : NotFound(result.Errors[0].Message);
         }
 
+        /// <summary>
+        /// Returns a list of cats with optional tag filter and pagination.
+        /// </summary>
+        /// <param name="tag">Optional tag name to filter cats by.</param>
+        /// <param name="page">The page number.</param>
+        /// <param name="pageSize">Number of results per page.</param>
+        /// <returns>A paginated list of cats.</returns>
+        /// <response code="200">Returns the list of cats</response>
+        /// <response code="404">No cats found</response>
         [HttpGet]
         public async Task<IActionResult> GetCats([FromQuery] string? tag, [FromQuery] int page = 1, [FromQuery] int pageSize = 10) {
             var result = await catService.GetCatsAsync(tag, page, pageSize);
--- Obtain_25_Cat_Images/Obtain_25_Cat_Images/Extentions/ApplicationServiceExtensions.cs
+++ Obtain_25_Cat_Images/Obtain_25_Cat_Images/Extentions/ApplicationServiceExtensions.cs
@@ -2,15 +2,32 @@
 using Microsoft.EntityFrameworkCore;
 using Obtain_25_Cat_Images.Interfaces;
 using Obtain_25_Cat_Images.Services;
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 
 namespace Obtain_25_Cat_Images.Extentions {
 
     public static class ApplicationServiceExtensions {
-
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config) {
             services.AddControllers();
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "Steal 25 Cats API",
+                    Version = "v1",
+                    Description = "REST API for fetching and managing cats from TheCatAPI.",
+                    Contact = new OpenApiContact {
+                        Name = "Sofia Doumani",
+                        Url = new Uri("https://github.com/DoumSof99")
+                    }
+                });
+
+            });
+
             services.AddDbContext<AppDbContext>(o => 
                 o.UseSqlServer(config.GetConnectionString("DefaultConnections")));
             services.AddHttpClient<ICatService, CatService>("TheCatApi", client => {
