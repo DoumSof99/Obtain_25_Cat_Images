b31209c24c59ede9701a59a56448f08432e04473
DoumSof99 <sofiadmn99@gmail.com>
2025-04-19 13:00:55 +0300

    Added more tests for FetchAndSaveCatsAsync()

--- Obtain_25_Cat_Images/Obtain_25_Cats_Tests/Services/CatServiceTests.cs
+++ Obtain_25_Cat_Images/Obtain_25_Cats_Tests/Services/CatServiceTests.cs
@@ -5,10 +5,12 @@ using Moq;
 using Moq.Protected;
 using Obtain_25_Cat_Images.Data;
 using Obtain_25_Cat_Images.DTOs;
+using Obtain_25_Cat_Images.Models.Entities;
 using Obtain_25_Cat_Images.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -16,13 +18,14 @@ using System.Threading.Tasks;
 namespace Obtain_25_Cats_Tests.Services {
     public class CatServiceTests {
 
+        #region Mock Data
         private readonly IMapper _mapper;
 
         public CatServiceTests() {
             var config = new MapperConfiguration(mc => {
                 mc.AddMaps("Obtain_25_Cat_Images");
             });
-            _mapper= config.CreateMapper();
+            _mapper = config.CreateMapper();
         }
 
         private static AppDbContext GetInMemoryDbContext() {
@@ -32,7 +35,7 @@ namespace Obtain_25_Cats_Tests.Services {
             return new AppDbContext(options);
         }
 
-        private static HttpClient GetMockHttpClient(List<CatApiResponseDTO> mockData) {
+        private static HttpClient GetMockHttpClient<T>(T mockData) {
             var json = JsonSerializer.Serialize(mockData);
             var msgHandlerMock = new Mock<HttpMessageHandler>();
 
@@ -51,8 +54,12 @@ namespace Obtain_25_Cats_Tests.Services {
             };
         }
 
+        #endregion
+
+        #region Tests for FetchAndSaveCatsAsync()
+
         [Fact]
-        public async Task Fetch_And_Save_Cats_Has_Cats_And_Tags() {
+        public async Task Valid_Save_Cats_And_Tags() {
             var db = GetInMemoryDbContext();
             var mockApiCats = new List<CatApiResponseDTO> {
                 new() {
@@ -86,7 +93,7 @@ namespace Obtain_25_Cats_Tests.Services {
             result.IsSuccess.Should().BeTrue();
             result.Value.Should().HaveCount(2);
 
-            var cat1 = result.Value[0];            
+            var cat1 = result.Value[0];
             cat1.Id.Should().Be("0XYvRd7oD");
             cat1.Image.Should().Be("https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg");
             cat1.Width.Should().Be(800);
@@ -102,5 +109,63 @@ namespace Obtain_25_Cats_Tests.Services {
             cat2.Tags.Select(t => t.Name).Should().Contain(["Intelligent", "Gentle"]);
         }
 
+        [Fact]
+        public async Task Should_Ignore_Duplicates_Cats() {
+            var db = GetInMemoryDbContext();
+            db.Cats.Add(new CatEntity { Id = 1, CatId = "CatId1", Image = "image234.jpg", Width = 300, Height = 120 });
+            db.SaveChanges();
+
+            var mockApiCats = new List<CatApiResponseDTO> {
+                new() { Id = "CatId1", Url = "https://image543.jpg", Width = 400, Height = 220 }
+            };
+            var httpClient = GetMockHttpClient(mockApiCats);
+            var service = new CatService(db, _mapper, httpClient);
+
+            var result = await service.FetchAndSaveCatsAsync();
+
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Fetch_Should_Fail_If_Response_Is_Null() {
+
+            var db = GetInMemoryDbContext();
+           
+            var httpClient = GetMockHttpClient<List<CatApiResponseDTO>>(null!);
+            var service = new CatService(db, _mapper, httpClient);
+
+            var result = await service.FetchAndSaveCatsAsync();
+
+            result.IsFailed.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Fetch_Cats_Should_Have_Tags_With_Empty_Name() {
+            var db = GetInMemoryDbContext();
+            var fakeApiCats = new List<CatApiResponseDTO>
+            {
+                new()
+                {
+                    Id = "cat_no_tags",
+                    Url = "img.jpg",
+                    Width = 300,
+                    Height = 300,
+                    Breads = [new() { Temperament = "" }]
+                }
+            };
+
+            var httpClient = GetMockHttpClient(fakeApiCats);
+            var service = new CatService(db, _mapper, httpClient);
+
+            var result = await service.FetchAndSaveCatsAsync();
+
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().NotBeNull();
+            result.Value.Single().Tags.Should().BeEmpty();
+        }
+
+
+        #endregion
     }
 }
